d0d807be1c5f02bdfc0c7f91d50f23b2f2a033cd
AzenaideFernandes <azenaidefkt@gmail.com>
2022-10-22 12:11:38 -0400

    Segundo commit do projeto

---
+++ src/main/java/trabalhofinal/delasbank/DelasbankApplication.java
@@ -0,0 +1,13 @@
+package trabalhofinal.delasbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelasbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelasbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/trabalhofinal/delasbank/DelasbankApplicationTests.java
@@ -0,0 +1,13 @@
+package trabalhofinal.delasbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelasbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
